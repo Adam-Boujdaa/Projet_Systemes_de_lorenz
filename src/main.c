07679b03357304436ce5d992d50987a1e0c26888
AdamB <adam.boujdaa@univ-tlse3.fr>
2024-12-04 22:48:08 +0100

    j'ai avance sur qques fonctions basiques

---
+++ src/main.c
@@ -0,0 +1,33 @@
+void ask_parametres_lorentz (){
+    int sigma;
+    int beta;
+    printf("Choisir parametre sigma : ");
+    scanf("%d", &sigma);
+    printf("Choisir parametre beta : ");
+    scanf("%d", &beta);
+}
+
+void ask_coordonnees(){
+    int x;
+    int y;
+    int z;
+    printf("Choisir coordonnee x : ");
+    scanf("%d", &x);
+    printf("Choisir coordonnee y : ");
+    scanf("%d", &y);
+    printf("Choisir coordonnee z : ");
+    scanf("%d", &z);
+}
+
+
+void choix_sys_dynamique( /*void ou struct ?*/ sys_choisi){   /*choisit le systeme dynamique, struct qquechose ?*/
+    /*void ou struct ?*/ sys_dyna = sys_chosi
+}
+
+int main (){
+    parametres_lorentz();
+    ask_coordonnees();
+
+
+    return 0;
+}
