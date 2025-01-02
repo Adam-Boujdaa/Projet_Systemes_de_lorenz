6b8ae79db0d683fecd15bbc9e72e0a87c9410dc1
AdamB <adam.boujdaa@univ-tlse3.fr>
2025-01-02 11:36:42 +0100

    corrige un truc pour pas que gnuplot se ferme automatiquement apres l'execution de lorenz.elf

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int main (){
 
     // Tracer avec Gnuplot
     printf("Simulation terminée. Visualisation avec Gnuplot...\n");
-    system("gnuplot -e \"set parametric; splot 'lorenz.dat' u 2:3:4\"");
+    system("gnuplot -e \"set parametric; splot 'lorenz.dat' u 2:3:4; pause -1\"");
 
 
     return 0;
